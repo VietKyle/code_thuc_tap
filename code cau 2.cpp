243302d72331aaf2e6e66fdf6c5f4e54362b2ef0
VietKyle <VietKyle@users.noreply.github.com>
2018-12-26 00:20:09 +0700

    Câu 2 hoàn chỉnh

--- code cau 2.cpp
+++ code cau 2.cpp
@@ -5,6 +5,7 @@ struct date{
 	int month;
 	int year;
 };
+int Thang[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
 int ktnhuan(int year){
 	if ((year % 4 == 0 && year % 100!=0) || year % 400 == 0)
 		return 1;
@@ -35,56 +36,70 @@ void NhapNgay(date &datetime){
 }
 
 int demngay(date datetime){
-	int d30=0,d31=0,d2;
-	if (ktnhuan(datetime.year)==1)
-		d2 = 29;
-	else d2 = 28;
-	int dem = datetime.day;
-	if (datetime.month == 1)
-		return dem;
-	else
-	for (int i=1;i<datetime.month;i++)
-		if (i==4 || i==6 || i==9 || i==11)
-			d30++;
-		else if(i==2)
-			dem+=d2;
-		else d31++;
-	return dem += d30*30 + d31*31;
+	int dem=0;
+	for(int i = 1; i < datetime.month; i++)
+		dem += Thang[i];
+	dem += datetime.day;
+	if (datetime.month>2)
+		if (ktnhuan(datetime.year)==1)
+			dem++;
+	return dem;
 }
 //--------------------------------------------------------------
-date addNgay(date datetime, int x){
-	int kt1 = x/31;
-	int kt2 = x%31;
-	int d30=0,d2=0;
-	date test;
-	test.month = datetime.month + kt1;
-	test.day = datetime.day + kt2;
-	test.year = datetime.year;
-	for (int i = datetime.month;i<test.month;i++){
-		if (i==4 || i==6 || i==9 || i==11)
-			d30++;
-		if (i==2)
-			if (ktnhuan(datetime.year)==1)
-				d2=1;
-			else d2=2;
+date TimNgay(int x,int year){
+	date NgayCanTim;
+	int i=1;
+	while (x > Thang[i]){
+		x-=Thang[i];
+		i++;
+		if (ktnhuan(year)==1 && i==2)
+			Thang[i]=29;
 	}
-	test.day -= (d30+d2);
-	if (test.day > 30){
-		if (ktnhuan(test.year)==1 && test.month==2)
-			test.day++;
-		test.day-=30;
-		test.month++;
+	NgayCanTim.month = i;
+	NgayCanTim.day = x;
+	NgayCanTim.year = year;
+	Thang[2]=28;
+	return NgayCanTim;
+}
+date ThemNgay(date datetime, int x){
+	int kq = x + demngay(datetime);
+	int year = datetime.year;
+	if(kq <= 365)
+		return TimNgay(kq,year);
+	else
+		if(ktnhuan(year) == 1)
+			if(kq == 366)
+				return TimNgay(kq,year);
+			else
+				return TimNgay(kq - 366, year + 1);
+		else
+			return TimNgay(kq - 365, year + 1);
 	}
-	return test;
-}	
+date TruNgay(date datetime, int x){
+	int kq,year;
+	if (x < demngay(datetime))
+		 kq = demngay(datetime)-x;
+	else {
+		year = datetime.year-1; 
+		kq = demngay(datetime) + 365 - x;
+		if (ktnhuan(datetime.year)==1 && (datetime.month>2 || (datetime.month==2 && datetime.day==29)))
+			kq++;
+		}
+		cout << kq <<endl;
+	return TimNgay(kq,year);
+}
+void InNgay(date datetime){
+	cout <<datetime.day << "/" << datetime.month << "/" <<datetime.year <<endl;
+}
 int main(){
 	date datetime;
 	NhapNgay(datetime);
 	cout << "nhap dung------------\n";
+	InNgay(datetime);
 	if(ktnhuan(datetime.year)==1)
 		cout <<"nam nay la nam nhuan\n";
 	else cout <<"nam ko nhuan\n";
 	cout << "la ngay thu " <<demngay(datetime) <<" trong nam\n";
-	cout << addNgay(datetime,50).day << "/" << addNgay(datetime,50).month <<"/"<< addNgay(datetime,50).year;
-	
+	InNgay(ThemNgay(datetime,20));
+	InNgay(TruNgay(datetime,61));
 }
