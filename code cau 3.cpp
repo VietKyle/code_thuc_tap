fea862ee258ddd303ceef990887f8103b12c53c7
VietKyle <VietKyle@users.noreply.github.com>
2018-12-28 03:05:43 +0700

    Code Câu 3 bản 3.2
    Chưa hoàn chỉnh

--- code cau 3.cpp
+++ code cau 3.cpp
@@ -16,13 +16,18 @@ Question CH;
 //FILE *f = fopen("de 1","rt");
 char De[5];
 void ChonDe();
+int diem;
+int BangDiem[50];
 ifstream f;
 void docDe();
-void QandA(); 
+void QandA(int &diem); 
+void Luudiem(int &diem);
 int main()
 {
 	ChonDe();
-	QandA();
+	diem = 0;
+	QandA(diem);
+	Luudiem(diem);
 }
 void ChonDe(){
 	cout << "nhap ma de (De 1, De 2, De 3, De 4):";
@@ -46,11 +51,10 @@ void docDe(){
 	f.close();
 	}
 }
-int QandA(){
+void QandA(int &diem){
 	docDe();
 	Question CH;
 	string s;
-	int diem=0;
 	for(int i=0;i<CauHoi.size();i++){
 		CH = CauHoi[i];
 		cout << "cau " << i+1 << ": "<< CH.Q <<endl;
@@ -63,6 +67,30 @@ int QandA(){
 		if (tl==s)
 			diem++;
 	}
-	cout << "diem:" <<diem;
+	cout << "diem:" <<diem<<"\n";
 	f.close();
+}
+void Luudiem(int &diem){
+	int dem = 0;
+	int s;
+	string st;
+	getline(f,st);
+	ifstream f("Diem.txt");
+	if (f!=NULL)
+	while (!f.eof()){
+		f >> s;
+		dem++;
+		BangDiem[dem] = s;
+	}
+	ofstream fo("Diem.txt");
+	dem++;
+	BangDiem[dem] = diem;
+	
+	cout << "diem luu:" << BangDiem[dem] << endl;
+	for (int i=1;i<=dem;i++){
+		cout << BangDiem[i] << "  ";
+		fo <<"\n"<< BangDiem[i] ;
+	}
+	f.close();
+	fo.close();
 }
